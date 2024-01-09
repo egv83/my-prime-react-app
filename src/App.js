85cd1d38baa894ef012e06a59ca358ebc165964e
egv83 <esteban8305@hotmail.com>
2024-01-08 23:22:30 -0500

    EVM - se agrega thema de prime react y primeicons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button} from 'primereact/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Button icon="pi pi-check" ></Button>
+        
+          <Button label='Click Me' icon="pi pi-check" ></Button>
+          
+          <Button label='Click Me' icon="pi pi-check" iconPos='right'></Button>
+
+        </div>
       </header>
     </div>
   );
